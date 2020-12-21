f97a0cea9d186bb5a130bbe8bf6bd130caba14a9
madeline <zzzz465@naver.com>
2020-12-21 22:41:03 +0900

    fix type declaration, vetur settings, etc

--- config-gui/.eslintrc.js
+++ config-gui/.eslintrc.js
@@ -13,8 +13,7 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    '@typescript-eslint/ban-ts-ignore': 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   },
   plugins: [
     "only-warn"
