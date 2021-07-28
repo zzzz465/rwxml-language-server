b303978d848bb91a5c2d12c61025be1e509db5f9
madeline <zzzz465@naver.com>
2021-07-28 23:41:24 +0900

    format utils/*.ts

--- language-server/src/utils/nodes.ts
+++ language-server/src/utils/nodes.ts
@@ -1,21 +1,19 @@
-
 import { Node } from '../parser/XMLParser'
 
 export function BFS(root: Node, excludeRoot?: boolean): Node[] {
-	const marker: Set<Node> = new Set()
-	if (excludeRoot !== true)
-		marker.add(root)
-	const queue: Node[] = [root]
-	while (queue.length > 0) {
-		const curr = queue.pop()!
+  const marker: Set<Node> = new Set()
+  if (excludeRoot !== true) marker.add(root)
+  const queue: Node[] = [root]
+  while (queue.length > 0) {
+    const curr = queue.pop()!
 
-		for (const child of curr.children) {
-			if (!marker.has(child)) {
-				marker.add(child)
-				queue.push(child)
-			}
-		}
-	}
+    for (const child of curr.children) {
+      if (!marker.has(child)) {
+        marker.add(child)
+        queue.push(child)
+      }
+    }
+  }
 
-	return [...marker.values()]
+  return [...marker.values()]
 }
--- language-server/src/utils/strings.ts
+++ language-server/src/utils/strings.ts
@@ -1,63 +1,57 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
 export function startsWith(haystack: string, needle: string): boolean {
-	if (haystack.length < needle.length) {
-		return false
-	}
+  if (haystack.length < needle.length) {
+    return false
+  }
 
-	for (let i = 0; i < needle.length; i++) {
-		if (haystack[i] !== needle[i]) {
-			return false
-		}
-	}
+  for (let i = 0; i < needle.length; i++) {
+    if (haystack[i] !== needle[i]) {
+      return false
+    }
+  }
 
-	return true
+  return true
 }
 
 /**
  * Determines if haystack ends with needle.
  */
 export function endsWith(haystack: string, needle: string): boolean {
-	const diff = haystack.length - needle.length
-	if (diff > 0) {
-		return haystack.lastIndexOf(needle) === diff
-	} else if (diff === 0) {
-		return haystack === needle
-	} else {
-		return false
-	}
+  const diff = haystack.length - needle.length
+  if (diff > 0) {
+    return haystack.lastIndexOf(needle) === diff
+  } else if (diff === 0) {
+    return haystack === needle
+  } else {
+    return false
+  }
 }
 
 /**
  * @returns the length of the common prefix of the two strings.
  */
 export function commonPrefixLength(a: string, b: string): number {
+  let i: number
+  const len = Math.min(a.length, b.length)
 
-	let i: number
-	const len = Math.min(a.length, b.length)
+  for (i = 0; i < len; i++) {
+    if (a.charCodeAt(i) !== b.charCodeAt(i)) {
+      return i
+    }
+  }
 
-	for (i = 0; i < len; i++) {
-		if (a.charCodeAt(i) !== b.charCodeAt(i)) {
-			return i
-		}
-	}
-
-	return len
+  return len
 }
 
 export function repeat(value: string, count: number) {
-	let s = ''
-	while (count > 0) {
-		if ((count & 1) === 1) {
-			s += value
-		}
-		value += value
-		count = count >>> 1
-	}
-	return s
+  let s = ''
+  while (count > 0) {
+    if ((count & 1) === 1) {
+      s += value
+    }
+    value += value
+    count = count >>> 1
+  }
+  return s
 }
 
 const _a = 'a'.charCodeAt(0)
@@ -68,6 +62,6 @@ const _0 = '0'.charCodeAt(0)
 const _9 = '9'.charCodeAt(0)
 
 export function isLetterOrDigit(text: string, index: number) {
-	const c = text.charCodeAt(index)
-	return (_a <= c && c <= _z) || (_A <= c && c <= _Z) || (_0 <= c && c <= _9)
+  const c = text.charCodeAt(index)
+  return (_a <= c && c <= _z) || (_A <= c && c <= _Z) || (_0 <= c && c <= _9)
 }
