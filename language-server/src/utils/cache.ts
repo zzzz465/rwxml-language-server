fa1169d4a71eefdaf3dcd8ad9e35aef8b85d3e47
madeline <zzzz465@naver.com>
2021-08-08 18:13:04 +0900

    extractor: fix cache not working correctly

--- language-server/src/utils/cache.ts
+++ language-server/src/utils/cache.ts
@@ -1,26 +1,17 @@
 export function cache(): MethodDecorator {
-  let cached = false
-  let value: any = undefined
-
   return function (target, propertyKey, descriptor: PropertyDescriptor) {
     // eslint-disable-next-line @typescript-eslint/ban-types
     const original = (<unknown>descriptor.value) as Function
+    let cached = false
+    let value: any = undefined
 
     descriptor.value = function (...args: any[]) {
       if (!cached) {
-        value = original.apply(target, args)
-        descriptor.value = function () {
-          return value
-        }
+        value = original.apply(this, args)
         cached = true
-        return value
-      } else {
-        throw new Error(
-          `exception on behaviour: value cached but function is not replaced, object: ${target}, property: ${String(
-            propertyKey
-          )}`
-        )
       }
+
+      return value
     }
   }
 }
