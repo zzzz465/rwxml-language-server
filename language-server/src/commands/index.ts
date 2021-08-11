31d9e975229a786573701d84e9fb43cbb4de975e
madeline <zzzz465@naver.com>
2021-08-11 22:42:48 +0900

    language-server: move command entry file

--- language-server/src/commands/index.ts
+++ language-server/src/commands/index.ts
@@ -1,6 +1,10 @@
 import { Command } from 'commander'
 import tr from './tr'
 
-export default function (command: Command): void {
-  command.addCommand(tr())
-}
+const program = new Command()
+
+program.version('0.0.1', '-v, --version')
+
+program.addCommand(tr())
+
+program.parse(process.argv)
