c4f267531e2a84669d3362a7317c8f6a7c2d9580
madeline <zzzz465@naver.com>
2021-08-13 00:57:26 +0900

    vsc-extension: initialize project

--- vsc-extension/src/index.ts
+++ vsc-extension/src/index.ts
@@ -1,5 +1,30 @@
-import { ExtensionContext } from "vscode";
+import { ExtensionContext } from 'vscode'
+import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient'
+
+let client: LanguageClient
 
 export async function activate(context: ExtensionContext): Promise<void> {
+  // initalize language server
+  client = await initServer()
 
+  // wait server to be ready
+  client.start()
+  await client.onReady()
+
+  // send event when project file changes
+}
+
+export function deactivate() {
+  if (client) {
+    return client.stop()
+  }
+}
+
+async function initServer() {
+  const serverOptions: ServerOptions = {}
+  const clientOptions: LanguageClientOptions = {}
+
+  const client = new LanguageClient('rwxml-language-server', 'RWXML Language Server', serverOptions, clientOptions)
+
+  return client
 }
