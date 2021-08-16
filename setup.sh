7ee4f96cb4b504f733ff7bf2ec0f4198fa08e2ce
madeline <zzzz465@naver.com>
2021-08-17 01:06:35 +0900

    update package, setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 ROOT=$(pwd)
 
 cd "$ROOT/analyzer"; yarn; yarn build
-cd "$ROOT/language-server"; yarn
+cd "$ROOT/language-server"; yarn; yarn build
 cd "$ROOT/vsc-extension"; yarn
 
 pre-commit install
