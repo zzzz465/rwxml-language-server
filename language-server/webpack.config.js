de2853a9881aa691c481c913f710ad603e72c5ba
madeline <zzzz465@naver.com>
2021-08-19 00:39:51 +0900

    language-server: change to webpack

---
+++ language-server/webpack.config.js
@@ -0,0 +1,49 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// webpack setting that is currently not used.
+'use strict'
+
+const path = require('path')
+const webpack = require('webpack')
+
+/**@type {import('webpack').Configuration}*/
+const config = {
+  target: 'node',
+  entry: './src/index.ts',
+  resolveLoader: {
+    modules: ['node_modules'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2',
+    devtoolModuleFilenameTemplate: '../[resource-path]',
+  },
+  devtool: 'source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.isWebpack': JSON.stringify(true),
+    }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.js'],
+    mainFields: ['module', 'jsnext:main', 'main'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              projectReferences: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+}
+
+module.exports = config
