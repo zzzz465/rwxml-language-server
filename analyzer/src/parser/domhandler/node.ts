6ec9e4e5e142270d562f1f54b530b102eb6d7732
madeline <zzzz465@naver.com>
2021-08-26 00:28:11 +0900

    analyzer: copy-paste source codes for customization

---
+++ analyzer/src/parser/domhandler/node.ts
@@ -0,0 +1,360 @@
+/* eslint-disable prettier/prettier */
+// original source code: https://github.com/fb55/domhandler
+// all rights goes to original author.
+
+import { ElementType, isTag as isTagRaw } from 'domelementtype'
+
+const nodeTypes = new Map<ElementType, number>([
+  [ElementType.Tag, 1],
+  [ElementType.Script, 1],
+  [ElementType.Style, 1],
+  [ElementType.Directive, 1],
+  [ElementType.Text, 3],
+  [ElementType.CDATA, 4],
+  [ElementType.Comment, 8],
+  [ElementType.Root, 9],
+])
+
+/**
+ * This object will be used as the prototype for Nodes when creating a
+ * DOM-Level-1-compliant structure.
+ */
+export class Node {
+  /** Parent of the node */
+  parent: NodeWithChildren | null = null
+
+  /** Previous sibling */
+  prev: Node | null = null
+
+  /** Next sibling */
+  next: Node | null = null
+
+  /** The start index of the node. Requires `withStartIndices` on the handler to be `true. */
+  startIndex: number | null = null
+
+  /** The end index of the node. Requires `withEndIndices` on the handler to be `true. */
+  endIndex: number | null = null
+
+  /**
+   *
+   * @param type The type of the node.
+   */
+  constructor(public type: ElementType) {}
+
+  // Read-only aliases
+  get nodeType(): number {
+    return nodeTypes.get(this.type) ?? 1
+  }
+
+  // Read-write aliases for properties
+  get parentNode(): NodeWithChildren | null {
+    return this.parent
+  }
+
+  set parentNode(parent: NodeWithChildren | null) {
+    this.parent = parent
+  }
+
+  get previousSibling(): Node | null {
+    return this.prev
+  }
+
+  set previousSibling(prev: Node | null) {
+    this.prev = prev
+  }
+
+  get nextSibling(): Node | null {
+    return this.next
+  }
+
+  set nextSibling(next: Node | null) {
+    this.next = next
+  }
+
+  /**
+   * Clone this node, and optionally its children.
+   *
+   * @param recursive Clone child nodes as well.
+   * @returns A clone of the node.
+   */
+  cloneNode<T extends Node>(this: T, recursive = false): T {
+    return cloneNode(this, recursive)
+  }
+}
+
+/**
+ * A node that contains some data.
+ */
+export class DataNode extends Node {
+  /**
+   * @param type The type of the node
+   * @param data The content of the data node
+   */
+  constructor(type: ElementType.Comment | ElementType.Text | ElementType.Directive, public data: string) {
+    super(type)
+  }
+
+  get nodeValue(): string {
+    return this.data
+  }
+
+  set nodeValue(data: string) {
+    this.data = data
+  }
+}
+
+/**
+ * Text within the document.
+ */
+export class Text extends DataNode {
+  constructor(data: string) {
+    super(ElementType.Text, data)
+  }
+}
+
+/**
+ * Comments within the document.
+ */
+export class Comment extends DataNode {
+  constructor(data: string) {
+    super(ElementType.Comment, data)
+  }
+}
+
+/**
+ * Processing instructions, including doc types.
+ */
+export class ProcessingInstruction extends DataNode {
+  constructor(public name: string, data: string) {
+    super(ElementType.Directive, data)
+  }
+
+  'x-name'?: string
+  'x-publicId'?: string
+  'x-systemId'?: string
+}
+
+/**
+ * A `Node` that can have children.
+ */
+export class NodeWithChildren extends Node {
+  /**
+   * @param type Type of the node.
+   * @param children Children of the node. Only certain node types can have children.
+   */
+  constructor(
+    type: ElementType.Root | ElementType.CDATA | ElementType.Script | ElementType.Style | ElementType.Tag,
+    public children: Node[]
+  ) {
+    super(type)
+  }
+
+  // Aliases
+  get firstChild(): Node | null {
+    return this.children[0] ?? null
+  }
+
+  get lastChild(): Node | null {
+    return this.children.length > 0 ? this.children[this.children.length - 1] : null
+  }
+
+  get childNodes(): Node[] {
+    return this.children
+  }
+
+  set childNodes(children: Node[]) {
+    this.children = children
+  }
+}
+
+/**
+ * The root node of the document.
+ */
+export class Document extends NodeWithChildren {
+  constructor(children: Node[]) {
+    super(ElementType.Root, children)
+  }
+
+  'x-mode'?: 'no-quirks' | 'quirks' | 'limited-quirks'
+}
+
+/**
+ * The description of an individual attribute.
+ */
+interface Attribute {
+  name: string
+  value: string
+  namespace?: string
+  prefix?: string
+}
+
+/**
+ * An element within the DOM.
+ */
+export class Element extends NodeWithChildren {
+  /**
+   * @param name Name of the tag, eg. `div`, `span`.
+   * @param attribs Object mapping attribute names to attribute values.
+   * @param children Children of the node.
+   */
+  constructor(
+    public name: string,
+    public attribs: { [name: string]: string },
+    children: Node[] = [],
+    type: ElementType.Tag | ElementType.Script | ElementType.Style = name === 'script'
+      ? ElementType.Script
+      : name === 'style'
+        ? ElementType.Style
+        : ElementType.Tag
+  ) {
+    super(type, children)
+  }
+
+  // DOM Level 1 aliases
+  get tagName(): string {
+    return this.name
+  }
+
+  set tagName(name: string) {
+    this.name = name
+  }
+
+  get attributes(): Attribute[] {
+    return Object.keys(this.attribs).map((name) => ({
+      name,
+      value: this.attribs[name],
+      namespace: this['x-attribsNamespace']?.[name],
+      prefix: this['x-attribsPrefix']?.[name],
+    }))
+  }
+
+  'x-attribsNamespace'?: Record<string, string>
+  'x-attribsPrefix'?: Record<string, string>
+}
+
+/**
+ * @param node Node to check.
+ * @returns `true` if the node is a `Element`, `false` otherwise.
+ */
+export function isTag(node: Node): node is Element {
+  return isTagRaw(node)
+}
+
+/**
+ * @param node Node to check.
+ * @returns `true` if the node has the type `CDATA`, `false` otherwise.
+ */
+export function isCDATA(node: Node): node is NodeWithChildren {
+  return node.type === ElementType.CDATA
+}
+
+/**
+ * @param node Node to check.
+ * @returns `true` if the node has the type `Text`, `false` otherwise.
+ */
+export function isText(node: Node): node is Text {
+  return node.type === ElementType.Text
+}
+
+/**
+ * @param node Node to check.
+ * @returns `true` if the node has the type `Comment`, `false` otherwise.
+ */
+export function isComment(node: Node): node is DataNode {
+  return node.type === ElementType.Comment
+}
+
+/**
+ * @param node Node to check.
+ * @returns `true` if the node has the type `ProcessingInstruction`, `false` otherwise.
+ */
+export function isDirective(node: Node): node is ProcessingInstruction {
+  return node.type === ElementType.Directive
+}
+
+/**
+ * @param node Node to check.
+ * @returns `true` if the node has the type `ProcessingInstruction`, `false` otherwise.
+ */
+export function isDocument(node: Node): node is Document {
+  return node.type === ElementType.Root
+}
+
+/**
+ * @param node Node to check.
+ * @returns `true` if the node is a `NodeWithChildren` (has children), `false` otherwise.
+ */
+export function hasChildren(node: Node): node is NodeWithChildren {
+  return Object.prototype.hasOwnProperty.call(node, 'children')
+}
+
+/**
+ * Clone a node, and optionally its children.
+ *
+ * @param recursive Clone child nodes as well.
+ * @returns A clone of the node.
+ */
+export function cloneNode<T extends Node>(node: T, recursive = false): T {
+  let result: Node
+
+  if (isText(node)) {
+    result = new Text(node.data)
+  } else if (isComment(node)) {
+    result = new Comment(node.data)
+  } else if (isTag(node)) {
+    const children = recursive ? cloneChildren(node.children) : []
+    const clone = new Element(node.name, { ...node.attribs }, children)
+    children.forEach((child) => (child.parent = clone))
+
+    if (node['x-attribsNamespace']) {
+      clone['x-attribsNamespace'] = { ...node['x-attribsNamespace'] }
+    }
+    if (node['x-attribsPrefix']) {
+      clone['x-attribsPrefix'] = { ...node['x-attribsPrefix'] }
+    }
+
+    result = clone
+  } else if (isCDATA(node)) {
+    const children = recursive ? cloneChildren(node.children) : []
+    const clone = new NodeWithChildren(ElementType.CDATA, children)
+    children.forEach((child) => (child.parent = clone))
+    result = clone
+  } else if (isDocument(node)) {
+    const children = recursive ? cloneChildren(node.children) : []
+    const clone = new Document(children)
+    children.forEach((child) => (child.parent = clone))
+
+    if (node['x-mode']) {
+      clone['x-mode'] = node['x-mode']
+    }
+
+    result = clone
+  } else if (isDirective(node)) {
+    const instruction = new ProcessingInstruction(node.name, node.data)
+
+    if (node['x-name'] != null) {
+      instruction['x-name'] = node['x-name']
+      instruction['x-publicId'] = node['x-publicId']
+      instruction['x-systemId'] = node['x-systemId']
+    }
+
+    result = instruction
+  } else {
+    throw new Error(`Not implemented yet: ${node.type}`)
+  }
+
+  result.startIndex = node.startIndex
+  result.endIndex = node.endIndex
+  return result as T
+}
+
+function cloneChildren(childs: Node[]): Node[] {
+  const children = childs.map((child) => cloneNode(child, true))
+
+  for (let i = 1; i < children.length; i++) {
+    children[i].prev = children[i - 1]
+    children[i - 1].next = children[i]
+  }
+
+  return children
+}
---
+++ analyzer/src/parser/htmlparser2/parser.ts
@@ -0,0 +1,474 @@
+// source code: https://github.com/fb55/htmlparser2
+// all rights goes to original author.
+import Tokenizer from './Tokenizer'
+
+const formTags = new Set(['input', 'option', 'optgroup', 'select', 'button', 'datalist', 'textarea'])
+const pTag = new Set(['p'])
+const tableSectionTags = new Set(['thead', 'tbody'])
+const ddtTags = new Set(['dd', 'dt'])
+const rtpTags = new Set(['rt', 'rp'])
+
+const openImpliesClose = new Map<string, Set<string>>([
+  ['tr', new Set(['tr', 'th', 'td'])],
+  ['th', new Set(['th'])],
+  ['td', new Set(['thead', 'th', 'td'])],
+  ['body', new Set(['head', 'link', 'script'])],
+  ['li', new Set(['li'])],
+  ['p', pTag],
+  ['h1', pTag],
+  ['h2', pTag],
+  ['h3', pTag],
+  ['h4', pTag],
+  ['h5', pTag],
+  ['h6', pTag],
+  ['select', formTags],
+  ['input', formTags],
+  ['output', formTags],
+  ['button', formTags],
+  ['datalist', formTags],
+  ['textarea', formTags],
+  ['option', new Set(['option'])],
+  ['optgroup', new Set(['optgroup', 'option'])],
+  ['dd', ddtTags],
+  ['dt', ddtTags],
+  ['address', pTag],
+  ['article', pTag],
+  ['aside', pTag],
+  ['blockquote', pTag],
+  ['details', pTag],
+  ['div', pTag],
+  ['dl', pTag],
+  ['fieldset', pTag],
+  ['figcaption', pTag],
+  ['figure', pTag],
+  ['footer', pTag],
+  ['form', pTag],
+  ['header', pTag],
+  ['hr', pTag],
+  ['main', pTag],
+  ['nav', pTag],
+  ['ol', pTag],
+  ['pre', pTag],
+  ['section', pTag],
+  ['table', pTag],
+  ['ul', pTag],
+  ['rt', rtpTags],
+  ['rp', rtpTags],
+  ['tbody', tableSectionTags],
+  ['tfoot', tableSectionTags],
+])
+
+const voidElements = new Set([
+  'area',
+  'base',
+  'basefont',
+  'br',
+  'col',
+  'command',
+  'embed',
+  'frame',
+  'hr',
+  'img',
+  'input',
+  'isindex',
+  'keygen',
+  'link',
+  'meta',
+  'param',
+  'source',
+  'track',
+  'wbr',
+])
+
+const foreignContextElements = new Set(['math', 'svg'])
+
+const htmlIntegrationElements = new Set([
+  'mi',
+  'mo',
+  'mn',
+  'ms',
+  'mtext',
+  'annotation-xml',
+  'foreignObject',
+  'desc',
+  'title',
+])
+
+export interface ParserOptions {
+  /**
+   * Indicates whether special tags (`<script>`, `<style>`, and `<title>`) should get special treatment
+   * and if "empty" tags (eg. `<br>`) can have children.  If `false`, the content of special tags
+   * will be text only. For feeds and other XML content (documents that don't consist of HTML),
+   * set this to `true`.
+   *
+   * @default false
+   */
+  xmlMode?: boolean
+
+  /**
+   * Decode entities within the document.
+   *
+   * @default true
+   */
+  decodeEntities?: boolean
+
+  /**
+   * If set to true, all tags will be lowercased.
+   *
+   * @default !xmlMode
+   */
+  lowerCaseTags?: boolean
+
+  /**
+   * If set to `true`, all attribute names will be lowercased. This has noticeable impact on speed.
+   *
+   * @default !xmlMode
+   */
+  lowerCaseAttributeNames?: boolean
+
+  /**
+   * If set to true, CDATA sections will be recognized as text even if the xmlMode option is not enabled.
+   * NOTE: If xmlMode is set to `true` then CDATA sections will always be recognized as text.
+   *
+   * @default xmlMode
+   */
+  recognizeCDATA?: boolean
+
+  /**
+   * If set to `true`, self-closing tags will trigger the onclosetag event even if xmlMode is not set to `true`.
+   * NOTE: If xmlMode is set to `true` then self-closing tags will always be recognized.
+   *
+   * @default xmlMode
+   */
+  recognizeSelfClosing?: boolean
+
+  /**
+   * Allows the default tokenizer to be overwritten.
+   */
+  Tokenizer?: typeof Tokenizer
+}
+
+export interface Handler {
+  onparserinit(parser: Parser): void
+
+  /**
+   * Resets the handler back to starting state
+   */
+  onreset(): void
+
+  /**
+   * Signals the handler that parsing is done
+   */
+  onend(): void
+  onerror(error: Error): void
+  onclosetag(name: string): void
+  onopentagname(name: string): void
+  /**
+   *
+   * @param name Name of the attribute
+   * @param value Value of the attribute.
+   * @param quote Quotes used around the attribute. `null` if the attribute has no quotes around the value, `undefined` if the attribute has no value.
+   */
+  onattribute(name: string, value: string, quote?: string | undefined | null): void
+  onopentag(name: string, attribs: { [s: string]: string }): void
+  ontext(data: string): void
+  oncomment(data: string): void
+  oncdatastart(): void
+  oncdataend(): void
+  oncommentend(): void
+  onprocessinginstruction(name: string, data: string): void
+}
+
+const reNameEnd = /\s|\//
+
+export class Parser {
+  /** The start index of the last event. */
+  public startIndex = 0
+  /** The end index of the last event. */
+  public endIndex = 0
+
+  private tagname = ''
+  private attribname = ''
+  private attribvalue = ''
+  private attribs: null | { [key: string]: string } = null
+  private stack: string[] = []
+  private readonly foreignContext: boolean[] = []
+  private readonly cbs: Partial<Handler>
+  private readonly lowerCaseTagNames: boolean
+  private readonly lowerCaseAttributeNames: boolean
+  private readonly tokenizer: Tokenizer
+
+  constructor(cbs?: Partial<Handler> | null, private readonly options: ParserOptions = {}) {
+    this.options = options
+    this.cbs = cbs ?? {}
+    this.lowerCaseTagNames = options.lowerCaseTags ?? !options.xmlMode
+    this.lowerCaseAttributeNames = options.lowerCaseAttributeNames ?? !options.xmlMode
+    this.tokenizer = new (options.Tokenizer ?? Tokenizer)(this.options, this)
+    this.cbs.onparserinit?.(this)
+  }
+
+  private updatePosition(offset: number) {
+    this.startIndex = this.tokenizer.getAbsoluteSectionStart() - offset
+    this.endIndex = this.tokenizer.getAbsoluteIndex()
+  }
+
+  // Tokenizer event handlers
+  ontext(data: string): void {
+    this.startIndex = this.tokenizer.getAbsoluteSectionStart()
+    this.endIndex = this.tokenizer.getAbsoluteIndex() - 1
+    this.cbs.ontext?.(data)
+  }
+
+  protected isVoidElement(name: string): boolean {
+    return !this.options.xmlMode && voidElements.has(name)
+  }
+
+  onopentagname(name: string): void {
+    this.updatePosition(1)
+
+    if (this.lowerCaseTagNames) {
+      name = name.toLowerCase()
+    }
+
+    this.emitOpenTag(name)
+  }
+
+  private emitOpenTag(name: string) {
+    this.tagname = name
+
+    const impliesClose = !this.options.xmlMode && openImpliesClose.get(name)
+
+    if (impliesClose) {
+      while (this.stack.length > 0 && impliesClose.has(this.stack[this.stack.length - 1])) {
+        const el = this.stack.pop()!
+        this.cbs.onclosetag?.(el)
+      }
+    }
+    if (!this.isVoidElement(name)) {
+      this.stack.push(name)
+      if (foreignContextElements.has(name)) {
+        this.foreignContext.push(true)
+      } else if (htmlIntegrationElements.has(name)) {
+        this.foreignContext.push(false)
+      }
+    }
+    this.cbs.onopentagname?.(name)
+    if (this.cbs.onopentag) this.attribs = {}
+  }
+
+  onopentagend(): void {
+    this.endIndex = this.tokenizer.getAbsoluteIndex()
+
+    if (this.attribs) {
+      this.cbs.onopentag?.(this.tagname, this.attribs)
+      this.attribs = null
+    }
+    if (this.cbs.onclosetag && this.isVoidElement(this.tagname)) {
+      this.cbs.onclosetag(this.tagname)
+    }
+    this.tagname = ''
+  }
+
+  onclosetag(name: string): void {
+    this.updatePosition(2)
+    if (this.lowerCaseTagNames) {
+      name = name.toLowerCase()
+    }
+    if (foreignContextElements.has(name) || htmlIntegrationElements.has(name)) {
+      this.foreignContext.pop()
+    }
+    if (this.stack.length && !this.isVoidElement(name)) {
+      let pos = this.stack.lastIndexOf(name)
+      if (pos !== -1) {
+        if (this.cbs.onclosetag) {
+          pos = this.stack.length - pos
+          while (pos--) {
+            // We know the stack has sufficient elements.
+            this.cbs.onclosetag(this.stack.pop() as string)
+          }
+        } else this.stack.length = pos
+      } else if (name === 'p' && !this.options.xmlMode) {
+        this.emitOpenTag(name)
+        this.closeCurrentTag()
+      }
+    } else if (!this.options.xmlMode && (name === 'br' || name === 'p')) {
+      this.emitOpenTag(name)
+      this.closeCurrentTag()
+    }
+  }
+
+  onselfclosingtag(): void {
+    if (
+      this.options.xmlMode ||
+      this.options.recognizeSelfClosing ||
+      this.foreignContext[this.foreignContext.length - 1]
+    ) {
+      this.closeCurrentTag()
+    } else {
+      this.onopentagend()
+    }
+  }
+
+  private closeCurrentTag() {
+    const name = this.tagname
+    this.onopentagend()
+    /*
+     * Self-closing tags will be on the top of the stack
+     * (cheaper check than in onclosetag)
+     */
+    if (this.stack[this.stack.length - 1] === name) {
+      this.cbs.onclosetag?.(name)
+      this.stack.pop()
+    }
+  }
+
+  onattribname(name: string): void {
+    if (this.lowerCaseAttributeNames) {
+      name = name.toLowerCase()
+    }
+    this.attribname = name
+  }
+
+  onattribdata(value: string): void {
+    this.attribvalue += value
+  }
+
+  onattribend(quote: string | undefined | null): void {
+    this.cbs.onattribute?.(this.attribname, this.attribvalue, quote)
+    if (this.attribs && !Object.prototype.hasOwnProperty.call(this.attribs, this.attribname)) {
+      this.attribs[this.attribname] = this.attribvalue
+    }
+    this.attribname = ''
+    this.attribvalue = ''
+  }
+
+  private getInstructionName(value: string) {
+    const idx = value.search(reNameEnd)
+    let name = idx < 0 ? value : value.substr(0, idx)
+
+    if (this.lowerCaseTagNames) {
+      name = name.toLowerCase()
+    }
+
+    return name
+  }
+
+  ondeclaration(value: string): void {
+    if (this.cbs.onprocessinginstruction) {
+      this.updatePosition(2)
+      const name = this.getInstructionName(value)
+      this.cbs.onprocessinginstruction(`!${name}`, `!${value}`)
+    }
+  }
+
+  onprocessinginstruction(value: string): void {
+    if (this.cbs.onprocessinginstruction) {
+      this.updatePosition(2)
+      const name = this.getInstructionName(value)
+      this.cbs.onprocessinginstruction(`?${name}`, `?${value}`)
+    }
+  }
+
+  oncomment(value: string): void {
+    this.updatePosition(4)
+    this.cbs.oncomment?.(value)
+    this.cbs.oncommentend?.()
+  }
+
+  oncdata(value: string): void {
+    this.updatePosition(1)
+    if (this.options.xmlMode || this.options.recognizeCDATA) {
+      this.cbs.oncdatastart?.()
+      this.cbs.ontext?.(value)
+      this.cbs.oncdataend?.()
+    } else {
+      this.oncomment(`[CDATA[${value}]]`)
+    }
+  }
+
+  onerror(err: Error): void {
+    this.cbs.onerror?.(err)
+  }
+
+  onend(): void {
+    if (this.cbs.onclosetag) {
+      for (let i = this.stack.length; i > 0; this.cbs.onclosetag(this.stack[--i]));
+    }
+    this.cbs.onend?.()
+  }
+
+  /**
+   * Resets the parser to a blank state, ready to parse a new HTML document
+   */
+  public reset(): void {
+    this.cbs.onreset?.()
+    this.tokenizer.reset()
+    this.tagname = ''
+    this.attribname = ''
+    this.attribs = null
+    this.stack = []
+    this.cbs.onparserinit?.(this)
+  }
+
+  /**
+   * Resets the parser, then parses a complete document and
+   * pushes it to the handler.
+   *
+   * @param data Document to parse.
+   */
+  public parseComplete(data: string): void {
+    this.reset()
+    this.end(data)
+  }
+
+  /**
+   * Parses a chunk of data and calls the corresponding callbacks.
+   *
+   * @param chunk Chunk to parse.
+   */
+  public write(chunk: string): void {
+    this.tokenizer.write(chunk)
+  }
+
+  /**
+   * Parses the end of the buffer and clears the stack, calls onend.
+   *
+   * @param chunk Optional final chunk to parse.
+   */
+  public end(chunk?: string): void {
+    this.tokenizer.end(chunk)
+  }
+
+  /**
+   * Pauses parsing. The parser won't emit events until `resume` is called.
+   */
+  public pause(): void {
+    this.tokenizer.pause()
+  }
+
+  /**
+   * Resumes parsing after `pause` was called.
+   */
+  public resume(): void {
+    this.tokenizer.resume()
+  }
+
+  /**
+   * Alias of `write`, for backwards compatibility.
+   *
+   * @param chunk Chunk to parse.
+   * @deprecated
+   */
+  public parseChunk(chunk: string): void {
+    this.write(chunk)
+  }
+  /**
+   * Alias of `end`, for backwards compatibility.
+   *
+   * @param chunk Optional final chunk to parse.
+   * @deprecated
+   */
+  public done(chunk?: string): void {
+    this.end(chunk)
+  }
+}
