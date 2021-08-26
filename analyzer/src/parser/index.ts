df8e763826f8802a05a1dcc5aebd71efac81cf13
madeline <zzzz465@naver.com>
2021-08-26 20:06:51 +0900

    analyzer: remove unused projects, add helper functions

--- analyzer/src/parser/index.ts
+++ analyzer/src/parser/index.ts
@@ -1,2 +1,16 @@
+import DomHandler from './domhandler'
+import { Parser } from './htmlparser2'
+
 export * from './domhandler'
 export * from './htmlparser2'
+
+export function parse(text: string, uri = '') {
+  const domHandler = new DomHandler()
+  const parser = new Parser(domHandler)
+
+  parser.end(text)
+
+  domHandler.root.uri = uri
+
+  return domHandler.root
+}
