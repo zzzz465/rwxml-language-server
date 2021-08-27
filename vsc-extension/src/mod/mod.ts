50f24affabfe642eeafece7ca3e6517ee366d505
madeline <zzzz465@naver.com>
2021-08-28 06:29:08 +0900

    vsc-extension: WIP: implementing getDependencyFiles()

--- vsc-extension/src/mod/mod.ts
+++ vsc-extension/src/mod/mod.ts
@@ -3,6 +3,10 @@ import { About } from './about'
 import vscode from 'vscode'
 import { LoadFolder } from './loadFolders'
 import path from 'path'
+import { RimWorldVersion } from './version'
+import glob from 'fast-glob'
+import fs from 'fs/promises'
+import fsSync from 'fs'
 
 export class Mod {
   static async create(rootDirectory: Uri) {
@@ -29,4 +33,48 @@ export class Mod {
     public readonly about: About,
     public readonly loadFolder?: LoadFolder
   ) {}
+
+  getDependencyFiles(version?: RimWorldVersion) {
+    if (version && this.loadFolder) {
+      const dirs = this.loadFolder.getRequiredPaths(version)
+      return dirs.map((dir) => this.getDependencyFilesFromDirectory(Uri.file(dir)))
+    } else {
+      return [this.getDependencyFilesFromDirectory(this.rootDirectory)]
+    }
+  }
+
+  private async getDependencyFilesFromDirectory(directoryUri: Uri) {
+    type retType = {
+      defs?: { uri: Uri; text: string }[]
+    }
+    const ret: retType = {}
+
+    // check Defs
+    const defsPath = path.resolve(directoryUri.fsPath, 'Defs')
+    if (fsSync.existsSync(defsPath)) {
+      const defs = await this.getXMLDependencyFiles(Uri.file(defsPath))
+      ret.defs = defs
+    }
+
+    // check Texture
+
+    // check Sound
+
+    // check etc...??
+
+    return ret
+  }
+
+  private async getXMLDependencyFiles(defsDirectoryUri: Uri) {
+    const urls = await glob('**/*.xml', { cwd: defsDirectoryUri.fsPath, absolute: true })
+
+    const promises = urls.map(async (url) => {
+      const text = await fs.readFile(url, { encoding: 'utf-8' })
+      return { uri: Uri.file(url), text }
+    })
+
+    const files = await Promise.all(promises)
+
+    return files
+  }
 }
