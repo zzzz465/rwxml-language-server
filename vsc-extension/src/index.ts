7552165aae22ce3f2d7d7cba620575ab011009bd
madeline <zzzz465@naver.com>
2021-08-28 06:45:46 +0900

    vsc-extension: WIP: add modManager on extension init, add hook of it

--- vsc-extension/src/index.ts
+++ vsc-extension/src/index.ts
@@ -5,10 +5,13 @@ import * as path from 'path'
 import vscode from 'vscode'
 import { updateDecoration } from './features'
 import { ProjectFileAdded, ProjectFileChanged, ProjectFileDeleted, WorkspaceInitialization } from './events'
+import { ModManager } from './mod/modManager'
+import { getWorkshopModsDirectoryUri } from './mod'
 
 let client: LanguageClient
 let disposed = false
 let fileSystemWatcher: FileSystemWatcher
+let modManager: ModManager
 const disposables: Disposable[] = []
 
 export async function activate(context: ExtensionContext): Promise<void> {
@@ -57,6 +60,13 @@ export async function activate(context: ExtensionContext): Promise<void> {
   )
 
   console.log('registering client features done.')
+
+  console.log('initializing modManager...')
+  const workshopModsDirectoryUri = getWorkshopModsDirectoryUri()
+  console.log(`current workshop Directory: ${decodeURIComponent(workshopModsDirectoryUri.toString())}`)
+  modManager = new ModManager([workshopModsDirectoryUri])
+  await modManager.init()
+  console.log('initializing modManager completed.')
 
   // wait server to be ready
   console.log('waiting language-server to be ready...')
--- vsc-extension/src/mod/index.ts
+++ vsc-extension/src/mod/index.ts
@@ -1,3 +1,29 @@
+import { Uri } from 'vscode'
+import process from 'process'
+
 export * from './about'
 export * from './mod'
 export * from './loadFolders'
+
+export function getWorkshopModsDirectoryUri(): Uri {
+  // TODO: get from config or env or something else...
+
+  // default
+  return getDefaultWorkshopModsDirectoryUri()
+}
+
+function getDefaultWorkshopModsDirectoryUri(): Uri {
+  switch (process.platform) {
+    case 'win32':
+      return Uri.file('C:\\Program Files (x86)\\Steam\\steamapps\\workshop\\content\\294100')
+
+    case 'darwin':
+      throw new Error('platform drawin is not supported YET. please make an issue.')
+
+    case 'linux':
+      throw new Error('platform linux is not supported YET. please make an issue.')
+
+    default:
+      throw new Error(`current platform: ${process.platform} is not supported.`)
+  }
+}
