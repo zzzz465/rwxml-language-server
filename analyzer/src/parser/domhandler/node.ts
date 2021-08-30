488de4a6c21f36d5c916b4ec364d4a478adecdc2
madeline <zzzz465@naver.com>
2021-08-31 00:48:52 +0900

    analyzer: fix nodeRange, dataRange of Text/Comment not parsed correctly

--- analyzer/src/parser/domhandler/node.ts
+++ analyzer/src/parser/domhandler/node.ts
@@ -93,10 +93,8 @@ export class Node {
  * A node that contains some data.
  */
 export class DataNode extends Node {
-  get nodeRange(): Range {
-    return this.dataRange
-  }
-  readonly dataRange: Range = new Range()
+  readonly nodeRange = new Range()
+  readonly dataRange = new Range()
 
   /**
    * @param type The type of the node
