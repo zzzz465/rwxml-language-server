75881ea1e04ba085c852a43e6793e2aa515acd8e
madeline <zzzz465@naver.com>
2021-09-15 22:11:52 +0900

    vsc-extension: fix eslint not working

--- vsc-extension/.eslintrc.js
+++ vsc-extension/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   root: true,
   env: {
     node: true,
