d4ffe839199bbf6d1c158fe5e0a420a797d21a55
madeline <zzzz465@naver.com>
2022-02-13 20:18:49 +0900

    client: implement textProvider

--- vsc-extension/src/resources/textProvider.ts
+++ vsc-extension/src/resources/textProvider.ts
@@ -1,6 +1,8 @@
 import { registry } from 'tsyringe'
 import { LanguageClient } from 'vscode-languageclient'
+import { TextRequest, TextRequestResponse } from '../events'
 import { Provider, ProviderSymbol } from './type'
+import * as vscode from 'vscode'
 
 @registry([
   {
@@ -9,7 +11,18 @@ import { Provider, ProviderSymbol } from './type'
   },
 ])
 export class TextProvider implements Provider {
-  listen(client: LanguageClient): void {
-    throw new Error('Method not implemented.')
+  async listen(client: LanguageClient): Promise<void> {
+    await client.onReady()
+    client.onRequest(TextRequest, this.onTextRequest.bind(this))
+  }
+
+  private async onTextRequest({ uri }: TextRequest): Promise<TextRequestResponse> {
+    try {
+      const data = await vscode.workspace.fs.readFile(vscode.Uri.parse(uri))
+      const xml = Buffer.from(data).toString('utf-8')
+      return { data: xml }
+    } catch (err) {
+      return { data: '', error: String(err) }
+    }
   }
 }
