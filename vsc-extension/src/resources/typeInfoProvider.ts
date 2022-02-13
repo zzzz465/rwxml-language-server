675704faaeca0fc552eb7762391fe83893dd3205
madeline <zzzz465@naver.com>
2022-02-13 20:55:21 +0900

    client: implement typeInfoProvider

--- vsc-extension/src/resources/typeInfoProvider.ts
+++ vsc-extension/src/resources/typeInfoProvider.ts
@@ -1,6 +1,9 @@
 import { registry } from 'tsyringe'
 import { LanguageClient } from 'vscode-languageclient'
+import { TypeInfoRequest, TypeInfoRequestResponse } from '../events'
+import { extractTypeInfos } from '../typeInfo'
 import { Provider, ProviderSymbol } from './type'
+import * as vscode from 'vscode'
 
 @registry([
   {
@@ -9,7 +12,20 @@ import { Provider, ProviderSymbol } from './type'
   },
 ])
 export class TypeInfoProvider implements Provider {
-  listen(client: LanguageClient): void {
-    throw new Error('Method not implemented.')
+  async listen(client: LanguageClient): Promise<void> {
+    await client.onReady()
+    client.onRequest(TypeInfoRequest, this.onTypeInfoRequest.bind(this))
+  }
+
+  private async onTypeInfoRequest({ uris }: TypeInfoRequest): Promise<TypeInfoRequestResponse> {
+    try {
+      const fsPaths = uris.map((uri) => vscode.Uri.parse(uri).fsPath)
+
+      const typeInfos = await extractTypeInfos(...fsPaths)
+
+      return { data: typeInfos }
+    } catch (err) {
+      return { error: String(err) }
+    }
   }
 }
