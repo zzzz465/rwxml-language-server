7650153808fe1274d27c31904f696a316712daf0
madeline <zzzz465@naver.com>
2022-02-16 23:49:33 +0900

    server: add utility functions to Provider class

--- language-server/src/features/provider.ts
+++ language-server/src/features/provider.ts
@@ -1,20 +1,41 @@
-import { container, Lifecycle, registry } from 'tsyringe'
-import { Connection } from 'vscode-languageserver'
+import * as tsyringe from 'tsyringe'
+import * as ls from 'vscode-languageserver'
+import { URI } from 'vscode-uri'
 import winston from 'winston'
+import { LoadFolder } from '../mod/loadfolders'
+import { ProjectManager } from '../projectManager'
 import { HoverProvider } from './hover'
 
-@registry([
+@tsyringe.registry([
   {
     token: Provider.token,
     useClass: HoverProvider,
-    options: { lifecycle: Lifecycle.Singleton },
+    options: { lifecycle: tsyringe.Lifecycle.Singleton },
   },
 ])
 export abstract class Provider {
   static readonly token = Symbol('LanguageFeatureProviderToken')
 
-  abstract listen(connection: Connection): void
+  constructor(private readonly loadFolder: LoadFolder, private readonly projectManager: ProjectManager) {}
+
+  abstract listen(connection: ls.Connection): void
   protected abstract getLogger(): winston.Logger
+
+  protected getProjects(uri: string | URI) {
+    const versions = this.getVersions(uri)
+
+    return versions.map((ver) => this.projectManager.getProject(ver))
+  }
+
+  protected getVersions(uri: string | URI) {
+    if (typeof uri === 'string') {
+      uri = URI.parse(uri)
+    }
+
+    const versions = this.loadFolder.isBelongsTo(uri)
+
+    return versions
+  }
 
   protected wrapExceptionStackTraces<P, R>(func: (arg: P) => Promise<R>): (arg: P) => Promise<R | undefined> {
     return async (arg: P) => {
@@ -26,8 +47,8 @@ export abstract class Provider {
     }
   }
 
-  static listenAll(connection: Connection): void {
-    const providers = container.resolveAll<Provider>(Provider.token)
+  static listenAll(connection: ls.Connection): void {
+    const providers = tsyringe.container.resolveAll<Provider>(Provider.token)
     for (const provider of providers) {
       provider.listen(connection)
     }
