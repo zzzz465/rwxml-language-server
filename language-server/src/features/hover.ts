1f178c2d74f94412c443bd9e36628688035d5942
madeline <zzzz465@naver.com>
2022-02-16 23:42:19 +0900

    server: listen onHover function

--- language-server/src/features/hover.ts
+++ language-server/src/features/hover.ts
@@ -2,6 +2,7 @@ import { injectable } from 'tsyringe'
 import { Connection } from 'vscode-languageserver'
 import { Provider } from './provider'
 import * as winston from 'winston'
+import * as ls from 'vscode-languageserver'
 
 @injectable()
 export class HoverProvider extends Provider {
@@ -12,7 +13,11 @@ export class HoverProvider extends Provider {
     return this.log
   }
 
-  listen(connection: Connection): Promise<void> {
-    throw new Error('Method not implemented.')
+  listen(connection: Connection): void {
+    connection.onHover(this.wrapExceptionStackTraces(this.onHover.bind(this)))
+  }
+
+  private onHover(p: ls.HoverParams): Promise<ls.Hover | undefined | null> {
+    throw new Error('method not implemented')
   }
 }
