a7b0e1221eb21f33c71b228dc5c7d4752bfe094a
madeline <zzzz465@naver.com>
2022-02-18 19:03:08 +0900

    server: project put file event to resourceStore without looking verisons

--- language-server/src/projectManager.ts
+++ language-server/src/projectManager.ts
@@ -1,32 +1,26 @@
 import EventEmitter from 'events'
-import { container, DependencyContainer, singleton } from 'tsyringe'
-import { URI } from 'vscode-uri'
-import { DependencyFile, File } from './fs'
+import * as tsyringe from 'tsyringe'
+import { File } from './fs'
 import { LoadFolder } from './mod/loadfolders'
 import { NotificationEvents } from './notificationEventManager'
 import { Project } from './project'
-import { ResourceStore } from './resourceStore'
-import { RimWorldVersion, RimWorldVersionToken } from './RimWorldVersion'
+import { RimWorldVersionToken } from './RimWorldVersion'
 import * as winston from 'winston'
 import { About } from './mod'
-
-// event that Projects will emit.
-interface ProjectManagerEvent {
-  fileAdded(version: string, file: File): void
-  fileChanged(version: string, file: File): void
-  fileDeleted(version: string, file: File): void
-}
-
 /**
  * ProjectManager manages DI container of specific rimworld version
  * and dispatch various events to each container
  */
-@singleton()
+@tsyringe.singleton()
 export class ProjectManager {
   private logFormat = winston.format.printf((info) => `[${info.level}] [${ProjectManager.name}] ${info.message}`)
   private readonly log = winston.createLogger({ transports: log.transports, format: this.logFormat })
 
-  private readonly projectContainers: Map<string, DependencyContainer> = new Map()
+  private readonly projectContainers: Map<string, tsyringe.DependencyContainer> = new Map()
+
+  get projects(): Project[] {
+    return [...this.projectContainers.values()].map((c) => c.resolve(Project))
+  }
 
   constructor(private readonly about: About, private readonly loadFolder: LoadFolder) {
     about.event.on('supportedVersionsChanged', this.onSupportedVersionsChanged.bind(this))
@@ -69,52 +63,28 @@ export class ProjectManager {
   private onProjectFileAdded(file: File) {
     this.log.debug(`file added: ${file.uri}`)
 
-    const versions = this.loadFolder.isBelongsTo(file.uri)
-    this.log.debug(`file's version is determined to: ${versions}`)
-
-    for (const version of versions) {
-      this.log.debug(`version: ${version}`)
-      const c = this.getOrCreateContainer(version)
-      c.resolve(ResourceStore).fileAdded(file)
+    for (const proj of this.projects) {
+      proj.resourceStore.fileAdded(file)
     }
   }
 
   private onProjectFileChanged(file: File) {
     this.log.debug(`file changed: ${file.uri}`)
 
-    const versions = this.loadFolder.isBelongsTo(file.uri)
-    this.log.debug(`file's version is determined to: ${versions}`)
-
-    for (const version of versions) {
-      this.log.debug(`version: ${version}`)
-      const c = this.getOrCreateContainer(version)
-      c.resolve(ResourceStore).fileChanged(file)
+    for (const proj of this.projects) {
+      proj.resourceStore.fileChanged(file)
     }
   }
 
   private onProjectFileDeleted(uri: string) {
     this.log.debug(`file deleted: ${uri}`)
 
-    const versions = this.loadFolder.isBelongsTo(URI.parse(uri))
-    this.log.debug(`file's version is determined to: ${versions}`)
-
-    for (const version of versions) {
-      this.log.debug(`version: ${version}`)
-      const c = this.getOrCreateContainer(version)
-      c.resolve(ResourceStore).fileDeleted(uri)
+    for (const proj of this.projects) {
+      proj.resourceStore.fileDeleted(uri)
     }
   }
 
-  private onContentChanged(file: File) {
-    const versions = this.loadFolder.isBelongsTo(file.uri)
-
-    for (const version of versions) {
-      const c = this.getOrCreateContainer(version)
-      c.resolve(ResourceStore).fileChanged(file)
-    }
-  }
-
-  private getOrCreateContainer(version: string): DependencyContainer {
+  private getOrCreateContainer(version: string): tsyringe.DependencyContainer {
     let projectContainer = this.projectContainers.get(version)
     if (!projectContainer) {
       projectContainer = this.newContainer(version)
@@ -124,63 +94,10 @@ export class ProjectManager {
     return projectContainer
   }
 
-  private newContainer(version: string): DependencyContainer {
-    const childContainer = container.createChildContainer()
+  private newContainer(version: string): tsyringe.DependencyContainer {
+    const childContainer = tsyringe.container.createChildContainer()
     childContainer.register(RimWorldVersionToken, { useValue: version })
 
     return childContainer
-  }
-}
-
-// checks event is acceptable, ignore if not.
-type ExcludeFirstParameter<F> = F extends (version: RimWorldVersion, ...args: infer P) => infer R
-  ? (...args: P) => R
-  : never
-
-type EventVersionFilterEvent = {
-  [event in keyof ProjectManagerEvent]: ExcludeFirstParameter<ProjectManagerEvent[event]>
-}
-
-/**
- * @deprecated don't filter events, just pass it directly
- */
-class EventVersionFilter {
-  public readonly event: EventEmitter<EventVersionFilterEvent> = new EventEmitter()
-  constructor(private readonly version: RimWorldVersion, private readonly loadFolders: LoadFolder) {}
-
-  listen(event: EventEmitter<ProjectManagerEvent>) {
-    event.on('fileAdded', this.onFileAdded.bind(this))
-    event.on('fileChanged', this.onFileChanged.bind(this))
-    event.on('fileDeleted', this.onFileDeleted.bind(this))
-  }
-
-  private onFileAdded(version: RimWorldVersion, file: File) {
-    if (this.ignoreFilter(file) || this.loadFolders.isBelongsTo(file.uri).includes(this.version)) {
-      this.event.emit('fileAdded', file)
-    }
-  }
-
-  private onFileChanged(version: RimWorldVersion, file: File) {
-    if (this.ignoreFilter(file) || this.loadFolders.isBelongsTo(file.uri).includes(this.version)) {
-      this.event.emit('fileChanged', file)
-    }
-  }
-
-  private onFileDeleted(version: RimWorldVersion, file: File) {
-    if (this.ignoreFilter(file) || this.loadFolders.isBelongsTo(file.uri).includes(this.version)) {
-      this.event.emit('fileDeleted', file)
-    }
-  }
-
-  private ignoreFilter(file: File) {
-    if (DependencyFile.is(file)) {
-      if (file.ownerPackageId.startsWith('Ludeon.RimWorld')) {
-        // core, royalty, ideology is loaded from local directory
-        // and it always have version "default"
-        return true
-      }
-    }
-
-    return false
   }
 }
