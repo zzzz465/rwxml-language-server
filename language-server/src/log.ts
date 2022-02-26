066200a5e885384c07b6e16ba1418309e07313c4
madeline <zzzz465@naver.com>
2022-02-26 13:55:24 +0900

    server: force debug level on development mode

--- language-server/src/log.ts
+++ language-server/src/log.ts
@@ -4,6 +4,10 @@ import * as winston from 'winston'
 export const LogToken = Symbol('LogToken')
 
 export function initializeLogger(level: string) {
+  if (process.env.NODE_ENV === 'development') {
+    level = 'debug'
+  }
+
   const log = winston.createLogger({
     transports: [new winston.transports.Console({ level })],
     format: winston.format.printf((info) => `[${info.level}] ${info.message}`),
