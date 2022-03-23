736babbf806ad5a9526c3b5c33da842ded65efab
zzzz465 <zzzz465@naver.com>
2022-03-23 23:33:28 +0900

    server: implement defListRequestHandler

---
+++ language-server/src/features/commands/defListRequestHandler.ts
@@ -0,0 +1,51 @@
+import * as tsyringe from 'tsyringe'
+import { Connection } from 'vscode-languageserver'
+import { Provider } from '../provider'
+import winston from 'winston'
+import { LogToken } from '../../log'
+import { ProjectHelper } from '../utils/project'
+import { ProjectManager } from '../../projectManager'
+import { DefListRequest, DefListRequestResponse } from '../../events'
+import { Def } from '@rwxml/analyzer'
+import { PlainObject } from '../../types/plainObject'
+import _ from 'lodash'
+import { AsEnumerable } from 'linq-es2015'
+
+@tsyringe.injectable()
+export class DefListRequestHandler implements Provider {
+  private logFormat = winston.format.printf((info) => `[${info.level}] [${DefListRequestHandler.name}] ${info.message}`)
+  private readonly log: winston.Logger
+
+  constructor(
+    private readonly projectManager: ProjectManager,
+    private readonly projectHelper: ProjectHelper,
+    @tsyringe.inject(LogToken) baseLogger: winston.Logger
+  ) {
+    this.log = winston.createLogger({ transports: baseLogger.transports, format: this.logFormat })
+  }
+
+  init(connection: Connection): void {
+    connection.onRequest(DefListRequest, this.projectHelper.wrapExceptionStackTraces(this.onRequest.bind(this)))
+  }
+
+  protected getLogger(): winston.Logger {
+    return this.log
+  }
+
+  private async onRequest({ version }: DefListRequest): Promise<DefListRequestResponse | null | undefined> {
+    const project = this.projectManager.getProject(version)
+
+    const marshalledDefs = AsEnumerable(project.defManager.defDatabase.defs())
+      .Select((def) => this.marshalDef(def))
+      .ToArray()
+
+    return { version, data: marshalledDefs }
+  }
+
+  private marshalDef(def: Def): PlainObject {
+    return {
+      type: def.getDefType(),
+      defName: def.getDefName() ?? '',
+    }
+  }
+}
