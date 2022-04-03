7d6e7d8769d26cbf11b84997a1dc943e8ee09727
zzzz465 <zzzz465@naver.com>
2022-04-04 01:01:37 +0900

    client: add zlibtypeInfoProvider

---
+++ vsc-extension/src/resources/zlibTypeInfoProvider.ts
@@ -0,0 +1,26 @@
+import * as tsyringe from 'tsyringe'
+import { CachedTypeInfoProvider } from './cachedTypeInfoProvider'
+import * as zlib from 'zlib'
+
+/**
+ * ZLibTypeInfoProvider provides CachedTypeInfoProvider with zlib compression.
+ */
+@tsyringe.injectable()
+export class ZLibTypeInfoProvider extends CachedTypeInfoProvider {
+  protected get compressionType(): string {
+    return 'zlib'
+  }
+
+  protected deserializeData(data: any): any {
+    const decompressed = zlib.gunzipSync(data)
+    const jsonText = decompressed.toString('utf-8')
+
+    return JSON.parse(jsonText)
+  }
+
+  protected serializeData(data: any): any {
+    const jsonText = JSON.stringify(data)
+    const compressed = zlib.gzipSync(jsonText)
+    return compressed.toString('utf-8')
+  }
+}
