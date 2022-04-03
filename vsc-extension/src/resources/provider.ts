0ce3868a4d80b756887a3861acd501e522f8aebd
zzzz465 <zzzz465@naver.com>
2022-04-04 01:02:03 +0900

    client: register zlibTypeInfoProvider as provider

--- vsc-extension/src/resources/provider.ts
+++ vsc-extension/src/resources/provider.ts
@@ -1,15 +1,15 @@
 import { registry } from 'tsyringe'
 import { LanguageClient } from 'vscode-languageclient'
-import { CachedTypeInfoProvider } from './cachedTypeInfoProvider'
 import { DependencyResourceProvider } from './dependencyResourceProvider'
 import { ResourceExistsProvider } from './resourceExistsProvider'
 import { TextProvider } from './textProvider'
+import { ZLibTypeInfoProvider } from './zlibTypeInfoProvider'
 
 @registry([
   { token: Provider.token, useClass: TextProvider },
   { token: Provider.token, useClass: DependencyResourceProvider },
   { token: Provider.token, useClass: ResourceExistsProvider },
-  { token: Provider.token, useClass: CachedTypeInfoProvider },
+  { token: Provider.token, useClass: ZLibTypeInfoProvider },
   // { token: Provider.token, useClass: TypeInfoProvider },
 ])
 export abstract class Provider {
