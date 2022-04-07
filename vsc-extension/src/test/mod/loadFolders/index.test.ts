f2c1b441bc0fdcb1559fd22c02ca57bc1f6600d0
zzzz465 <zzzz465@naver.com>
2022-04-08 01:34:12 +0900

    client: fix broken test

--- vsc-extension/src/test/mod/loadFolders/index.test.ts
+++ vsc-extension/src/test/mod/loadFolders/index.test.ts
@@ -1,3 +1,5 @@
+import 'reflect-metadata'
+
 import { mockVsCode } from '../../utils'
 mockVsCode()
 
@@ -8,16 +10,20 @@ import { LoadFolder } from '../../../mod'
 const loadFileUri = URI.file(path.resolve(__dirname, 'LoadFolders.xml'))
 
 describe('LoadFolder test', () => {
-  test('LoadFolder should be created on valid files', async () => {
+  test('LoadFolder should parse loadfolder as is if version is defined.', async () => {
     const loadFolder = await LoadFolder.Load(loadFileUri)
 
-    const ver1_0 = loadFolder.getRequiredPaths('1.0')
-    expect(ver1_0.length).toEqual(0)
+    const ver1_2 = loadFolder.getProjectWorkspace('1.2').relativePaths
+    expect(ver1_2).toEqual(['/', 'Current'].sort())
 
-    const ver1_2 = loadFolder.getRequiredPaths('1.2')
-    expect(ver1_2).toEqual(['/', 'Current'])
+    const ver1_3 = loadFolder.getProjectWorkspace('1.3').relativePaths
+    expect(ver1_3).toEqual(['/', 'Current'].sort())
+  })
 
-    const ver1_3 = loadFolder.getRequiredPaths('1.3')
-    expect(ver1_3).toEqual(['/', 'Current'])
+  test('loadfolder should return default workspace if not exists', async () => {
+    const loadFolder = await LoadFolder.Load(loadFileUri)
+
+    const ver1_0 = loadFolder.getProjectWorkspace('1.0').relativePaths
+    expect(ver1_0.sort()).toEqual(['1.0', '.'].sort())
   })
 })
