effa91e6c62a9037d9c1940c2f893bc8f31db836
madeline <zzzz465@naver.com>
2022-04-08 11:33:12 +0900

    client: send raw extraction command as base64encoded

--- vsc-extension/src/typeInfo/error.ts
+++ vsc-extension/src/typeInfo/error.ts
@@ -3,4 +3,19 @@ export class ExtractionError extends Error {
   constructor(message?: string, public readonly command?: string) {
     super(message)
   }
+
+  toJSON() {
+    return {
+      message: this.message,
+      command: this.getBase64EncodedCommand(),
+    }
+  }
+
+  getBase64EncodedCommand() {
+    if (this.command) {
+      return Buffer.from(this.command).toString('base64')
+    } else {
+      return ''
+    }
+  }
 }
