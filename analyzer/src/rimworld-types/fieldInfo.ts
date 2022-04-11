6d607c110fa2f1c7749ce04bdd863418381da49e
zzzz465 <zzzz465@naver.com>
2022-04-12 01:40:31 +0900

    analzyer, extractor: expose attribute value and ctor args

--- analyzer/src/rimworld-types/fieldInfo.ts
+++ analyzer/src/rimworld-types/fieldInfo.ts
@@ -1,14 +1,21 @@
 import { TypeInfo } from './typeInfo'
-import { TypeIdentifier } from './declaredType'
 
 export interface FieldInfoMetadata {}
+
+export interface FieldAttributeData {
+  attributeType: string
+  ctorArgs: {
+    type: string
+    value: string
+  }[]
+}
 
 export class FieldInfo {
   constructor(
     public readonly metadata: FieldInfoMetadata,
     public readonly declaringType: TypeInfo,
     public readonly fieldType: TypeInfo,
-    public readonly attributes: Record<string, TypeIdentifier>,
+    public readonly attributes: Record<string, FieldAttributeData>,
     public readonly isPublic: boolean,
     public readonly isPrivate: boolean,
     public readonly name: string
