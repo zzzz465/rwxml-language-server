73662e90a0ffe300aa637325010797a7f2e78076
zzzz465 <zzzz465@naver.com>
2022-04-12 23:14:07 +0900

    server: update textDocumentManager

--- language-server/src/textDocumentManager.ts
+++ language-server/src/textDocumentManager.ts
@@ -1,10 +1,13 @@
 import { inject, singleton } from 'tsyringe'
 import { TextDocument } from 'vscode-languageserver-textdocument'
-import { File, XMLFile } from './fs'
+import { File, TextFile, XMLFile } from './fs'
 import { NotificationEvents } from './notificationEventManager'
 import * as winston from 'winston'
 import { LogToken } from './log'
 import TypedEventEmitter from 'typed-emitter'
+import { FileStore } from './fileStore'
+import { Result } from './types/functional'
+import ono from 'ono'
 
 /**
  * TextDocumentManager manages all textDocuments
@@ -19,7 +22,7 @@ export class TextDocumentManager {
 
   private documents: Map<string, TextDocument> = new Map()
 
-  constructor(@inject(LogToken) baseLogger: winston.Logger) {
+  constructor(@inject(LogToken) baseLogger: winston.Logger, private readonly fileStore: FileStore) {
     this.log = baseLogger.child({ format: this.logFormat })
   }
 
@@ -29,27 +32,87 @@ export class TextDocumentManager {
     events.on('fileDeleted', (uri) => this.onFileDeleted(uri))
   }
 
-  get(uri: string) {
-    return this.documents.get(uri)
+  has(uri: string): boolean {
+    return this.documents.has(uri)
   }
 
-  private set(uri: string, text: string, language = 'xml') {
-    this.log.silly(`register document: uri: ${uri}, language: ${language}`)
-    const document = TextDocument.create(uri, language, 0, text)
+  async get(uri: string): Promise<Result<TextDocument, Error>> {
+    const [updateRequired, err0] = this.isUpdateRequired(uri)
+    if (err0) {
+      return [null, err0]
+    }
+
+    if (updateRequired) {
+      await this.updateDocument(uri)
+    }
+
+    const doc = this.documents.get(uri)
+    if (!doc) {
+      return [null, ono(`document not registered. uri: ${uri}`)]
+    }
+
+    return [doc, null]
+  }
+
+  async getText(uri: string): Promise<Result<string, Error>> {
+    const [doc, err] = await this.get(uri)
+    if (err) {
+      return [null, err]
+    }
+
+    return [doc?.getText(), null]
+  }
+
+  private isUpdateRequired(uri: string): Result<boolean, Error> {
+    const document = this.documents.get(uri)
+    if (!document) {
+      return [null, ono(`document not registered. uri: ${uri}`)]
+    }
+
+    const file = this.fileStore.get(uri)
+    if (!file) {
+      return [null, ono(`file not exists. uri: ${uri}`)]
+    }
+
+    return [document.version < file.updatedAt, null]
+  }
+
+  private set(uri: string, text: string, timestamp: number, language = 'xml'): TextDocument {
+    const document = TextDocument.create(uri, language, timestamp, text)
     this.documents.set(uri, document)
+
+    return document
+  }
+
+  private async updateDocument(uri: string): Promise<Result<string, Error>> {
+    const file = this.fileStore.get(uri)
+    if (!file) {
+      return [null, ono(`file not exists. uri: ${file}`)]
+    } else if (!(file instanceof TextFile)) {
+      return [null, ono(`file is not text file. uri: ${uri}`)]
+    }
+
+    let doc = this.documents.get(uri)
+    if (!doc) {
+      return [null, ono(`document is not registered. uri: ${uri}`)]
+    }
+
+    const data = await file.read()
+    if (doc.version < file.updatedAt) {
+      doc = this.set(uri, data, file.updatedAt)
+    }
+
+    return [doc.getText(), null]
   }
 
   private async onFileAdded(file: File): Promise<void> {
-    await this.onFileChanged(file)
+    if (file instanceof TextFile) {
+      this.set(file.uri.toString(), '', -1)
+    }
   }
 
   private async onFileChanged(file: File): Promise<void> {
-    if (file instanceof XMLFile) {
-      this.log.silly(`TextFile changed, uri: ${file.uri.toString()}`)
-
-      const data = await file.read()
-      this.set(file.uri.toString(), data)
-    }
+    // NOOP
   }
 
   private onFileDeleted(uri: string): void {
