71264a1a0b1b1df5f522df4b401a4b5d2289a5c3
madeline <zzzz465@naver.com>
2022-05-27 19:56:55 +0900

    server: add LogManager

--- language-server/src/log.ts
+++ language-server/src/log.ts
@@ -1,16 +1,52 @@
-import { container } from 'tsyringe'
-import * as winston from 'winston'
+import * as tsyringe from 'tsyringe'
+import winston, { format } from 'winston'
+import { Configuration } from './configuration'
 
-/*
-export const LogToken = Symbol('LogToken')
+const KEY_SCOPE = 'rwxml.logs'
+const KEY_LOG_LEVEL = 'level'
+const DEFAULT_LOG_LEVEL = 'info'
 
-export function initializeLogger(level: string) {
-  const log = winston.createLogger({
-    // transports: [],
-    transports: [new winston.transports.Console({ level })],
-    format: winston.format.printf((info) => `[${info.level}] ${info.message}`),
+export const DefaultLogToken = Symbol('DefaultLogToken')
+
+@tsyringe.singleton()
+export class LogManager {
+  readonly defaultLogger = winston.createLogger({
+    format: format.combine(logFormat),
+    level: DEFAULT_LOG_LEVEL,
+    transports: [new winston.transports.Console()],
   })
 
-  container.register(LogToken, { useValue: log })
+  constructor(private readonly conf: Configuration) {}
+
+  async init(): Promise<void> {
+    this.setLoggerLevel(await this.level())
+
+    this.conf.events.on('onConfigurationChanged', () => this.onConfigurationChanged())
+  }
+
+  async level(): Promise<string> {
+    return this.conf.get({ scopeUri: KEY_SCOPE, section: KEY_LOG_LEVEL }, DEFAULT_LOG_LEVEL)
+  }
+
+  private setLoggerLevel(level: string): void {
+    this.defaultLogger.level = level
+  }
+
+  private async onConfigurationChanged(): Promise<void> {
+    this.setLoggerLevel(await this.level())
+  }
 }
-*/
+
+export const className = format((info, classType?: new (...p: any[]) => any) => {
+  info.className = classType?.name ?? 'NONTYPE'
+
+  return info
+})
+
+export const logFormat = format.printf(({ level, className, id, message }) =>
+  id ? `[${level}]\t[${className}]\t(${id}):\t${message}` : `[${level}]\t[${className}]:\t${message}`
+)
+
+export default function defaultLogger() {
+  return tsyringe.container.resolve<winston.Logger>(DefaultLogToken)
+}
