0d65366992d0952726735d10e20c36f46fd5b7b7
madeline <zzzz465@naver.com>
2022-06-02 16:49:39 +0900

    server: fix Result<T>'s T not support null

--- language-server/src/utils/functional/result.ts
+++ language-server/src/utils/functional/result.ts
@@ -1,19 +1,15 @@
 import E from 'fp-ts/Either'
-import O from 'fp-ts/Option'
+import { flow } from 'fp-ts/lib/function'
+import { toNullable } from 'fp-ts/Option'
 import { ErrorLike } from 'ono'
 
-export type Result<T, E extends ErrorLike = ErrorLike> = E.Either<E, T>
+export namespace R {
+  export type Result<T, E extends ErrorLike = ErrorLike> = E.Either<E, T>
 
-const never = () => {
-  throw new Error()
+  export const fromNullable =
+    (err: ErrorLike) =>
+    <T>(x: T | null): Result<T> =>
+      x !== null ? E.right(x) : E.left(err)
+
+  export const fromOption = (err: ErrorLike) => flow(toNullable, fromNullable(err))
 }
-
-export const fromNullable =
-  (err: ErrorLike) =>
-  <T>(x: NonNullable<T> | null): Result<T> =>
-    x !== null ? E.right(x) : E.left(err)
-
-export const fromOption =
-  (err: ErrorLike) =>
-  <T extends O.Option<any>>(x: T): Result<T> =>
-    O.isSome(x) ? E.right(O.getOrElse(never as () => T)(x)) : E.left(err)
