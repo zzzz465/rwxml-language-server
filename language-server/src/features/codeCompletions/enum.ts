b66769b41724d1709f19eae4e02bbf7543b120cb
zzzz465 <zzzz465@naver.com>
2022-10-11 01:14:28 +0900

    server: fix suggestion not working on enum

--- language-server/src/features/codeCompletions/enum.ts
+++ language-server/src/features/codeCompletions/enum.ts
@@ -3,23 +3,32 @@ import * as tsyringe from 'tsyringe'
 import * as ls from 'vscode-languageserver'
 import { getMatchingText } from '../../data-structures/trie-ext'
 import { Project } from '../../project'
+import { expandUntil } from '../../utils/strings'
 import { isLeafNode } from '../utils'
 import { CodeCompletionContributor } from './contributor'
 
 @tsyringe.injectable()
 export class Enum implements CodeCompletionContributor {
-  getCompletion(_: Project, node: Node): ls.CompletionList | null {
+  getCompletion(_: Project, node: Node, offset: number): ls.CompletionList | null {
     if (node instanceof Text && node.parent instanceof Injectable) {
       node = node.parent as Injectable
     }
 
-    if (!(node instanceof Injectable) || !node.typeInfo.isEnum || !isLeafNode(node)) {
+    if (!(node instanceof Injectable) || !node.typeInfo.isEnum || !isLeafNode(node) || !node.contentRange) {
       return null
     }
 
     const content = node.content ?? ''
+    const relativeOffset = offset - node.contentRange.start
+    const currentContent = expandUntil(
+      content,
+      relativeOffset,
+      (c) => c !== ',',
+      (c) => c !== ','
+    )
+
     const haystacks = node.typeInfo.enums
-    const matchedTypes = getMatchingText(haystacks, content)
+    const matchedTypes = getMatchingText(haystacks, currentContent.text.trim())
     const items: ls.CompletionItem[] = matchedTypes.map((text) => ({
       label: text,
     }))
