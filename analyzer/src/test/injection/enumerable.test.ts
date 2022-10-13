458d29eee8090c665ffb86447e596f74979b52cd
madeline <zzzz465@naver.com>
2022-10-13 15:01:16 +0900

    analyzer: add enumerable type injection test

---
+++ analyzer/src/test/injection/enumerable.test.ts
@@ -0,0 +1,47 @@
+import { parse } from "../../parser"
+import { Injectable } from "../../rimworld-types"
+import { injector_1_4 } from "../data/injector"
+
+describe('enumerable type injection test', () => {
+  const xml = `
+  <?xml version="1.0" encoding="utf-8" ?>
+  <Defs>
+    <QuestScriptDef>
+      <defName>OpportunitySite_DownedRefugee</defName>
+      <root Class="QuestNode_Sequence">
+        <nodes>
+          <li Class="QuestNode_GetSitePartDefsByTagsAndFaction">
+            <storeAs>sitePartDefs</storeAs>
+            <storeFactionAs>siteFaction</storeFactionAs>
+            <sitePartsTags>
+              <li>
+                <tag>DownedRefugee</tag>
+              </li>
+              <li>
+                <tag>DownedRefugeeQuestThreat</tag>
+                <chance>$siteThreatChance</chance>
+              </li>
+            </sitePartsTags>
+          </li>
+        </nodes>
+      </root>
+    </QuestScriptDef>
+  </Defs>
+  `
+
+  const doc = parse(xml)
+  injector_1_4.inject(doc)
+
+  it('node should be treated as injectable if generic type is Enumerable', () => {
+    const rootNode = doc.findNodeAt(365)! as Injectable
+    expect(rootNode).toBeDefined()
+    expect(rootNode).toBeInstanceOf(Injectable)
+    expect(rootNode.typeInfo.className).toBe('SlateRef`1') // `1 = one generic argument
+    expect(rootNode.typeInfo.isEnumerable()).toBeTruthy()
+
+    const liNode = doc.findNodeAt(394)! as Injectable
+    expect(liNode).toBeDefined()
+    expect(liNode).toBeInstanceOf(Injectable)
+    expect(liNode.typeInfo.className).toBe('QuestNode_GetSitePartDefsByTagsAndFaction.SitePartOption')
+  })
+})
