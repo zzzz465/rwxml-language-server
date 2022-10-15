c0ac18dec653e77d770fa99a9964e7e36f7da308
madeline <zzzz465@naver.com>
2022-10-16 08:56:43 +0900

    analyzer: export missing types

--- analyzer/src/parser/domhandler/index.ts
+++ analyzer/src/parser/domhandler/index.ts
@@ -1,10 +1,11 @@
 // source code: https://github.com/fb55/domhandler
 // all rights goes to original author.
 
-export * from './node'
-export * from './element'
-export * from './utils'
 export * from './attribute'
+export * from './comment'
 export * from './document'
 export * from './domHandler'
 export * from './element'
+export * from './node'
+export * from './text'
+export * from './utils'
