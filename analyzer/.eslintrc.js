6afa671fb3b4ee6e4c10c58bae86340e3acc77ee
madeline <zzzz465@naver.com>
2022-10-15 21:25:46 +0900

    analyzer: fix wrong eslint rule name

--- analyzer/.eslintrc.js
+++ analyzer/.eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-namespace': 'off',
-    '@typescript-eslint/no-implicit-return-type': ['error'],
+    '@typescript-eslint/explicit-function-return-type': ['error'],
 
     indent: ['error', 2, { SwitchCase: 1 }],
     'linebreak-style': ['error', 'unix'],
