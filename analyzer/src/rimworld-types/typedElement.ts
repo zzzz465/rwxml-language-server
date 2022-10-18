e3ddca7eb5a60924788549e92184f081d6a4f894
zzzz465 <zzzz465@naver.com>
2022-10-18 17:13:08 +0900

    analyzer: embed Def inside typedElement.ts

--- analyzer/src/rimworld-types/typedElement.ts
+++ analyzer/src/rimworld-types/typedElement.ts
@@ -1,10 +1,9 @@
 import { cache, CacheScope, CacheType } from 'cache-decorator'
+import { ElementType } from 'domelementtype'
 import { Attribute } from '../parser/node/attribute'
 import { Element } from '../parser/node/element'
-import { Node } from '../parser/node/node'
-import { Def } from './def'
-import { FieldInfo } from './fieldInfo'
-import { TypeInfo } from './types'
+import { Node, NodeWithChildren } from '../parser/node/node'
+import { FieldInfo, TypeInfo } from './types'
 
 export class TypedElement extends Element {
   readonly typeInfo: TypeInfo
@@ -49,5 +48,70 @@ export class TypedElement extends Element {
 
   getFieldInfo(): FieldInfo | null {
     return this.fieldInfo
+  }
+}
+
+export class Def extends Element {
+  readonly typeInfo: TypeInfo
+  // required to implement TypedElement
+  readonly fieldInfo: null = null
+  readonly fields: Map<string, TypedElement>
+
+  constructor(
+    name: string,
+    attribs: { [name: string]: Attribute },
+    typeInfo: TypeInfo,
+    parent?: NodeWithChildren,
+    children?: Node[]
+  ) {
+    super(name, attribs, children, ElementType.Tag)
+
+    this.typeInfo = typeInfo
+    this.fields = new Map() // TODO: impl this
+    this.parent = parent ?? null
+  }
+
+  getDefType(): string {
+    const defType = this.typeInfo.getDefType()
+    if (defType) {
+      return defType
+    } else {
+      throw new Error(
+        `node: ${this.tagName} is DefType, but typeInfo doesn't have defType. uri: ${this.document.uri}, start: ${this.nodeRange.start}, end: ${this.nodeRange.end}`
+      )
+    }
+  }
+
+  isLeafNode(): boolean {
+    return this.children.length == 0
+  }
+
+  @cache({ scope: CacheScope.INSTANCE, type: CacheType.MEMO })
+  getDefName(): string | undefined {
+    const defNameElement = this.children.find((d: any) => (d as Element).tagName == 'defName') as Element | undefined
+    return defNameElement?.content
+  }
+
+  @cache({ scope: CacheScope.INSTANCE, type: CacheType.MEMO })
+  getNameAttributeValue(): string | undefined {
+    const inheritName = this.attribs['Name']
+    return inheritName?.value ?? undefined
+  }
+
+  @cache({ scope: CacheScope.INSTANCE, type: CacheType.MEMO })
+  getParentNameAttributeValue(): string | undefined {
+    const parentName = this.attribs['ParentName']
+    return parentName?.value ?? undefined
+  }
+
+  @cache({ scope: CacheScope.INSTANCE, type: CacheType.MEMO })
+  getDefPath(): string | null {
+    const defName = this.getDefName()
+
+    if (defName) {
+      return this.tagName + '.' + defName
+    }
+
+    return null
   }
 }
