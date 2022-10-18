f3f80b649ecd3b5f5d3757cb3320e652f20dfa66
zzzz465 <zzzz465@naver.com>
2022-10-18 18:10:18 +0900

    analyzer: add cheerio API test

---
+++ analyzer/src/test/parser/compability.test.ts
@@ -0,0 +1,27 @@
+import $ from 'cheerio'
+import { Element, parse } from '../../parser'
+
+describe('cheerio compability test', () => {
+  test('node should work with cheerio API', () => {
+    const xml = `
+<?xml version="1.0" encoding="utf-8"?>
+<Defs>
+    <ThingDef ParentName="BaseFilth">
+    <defName>Filth_Trash</defName>
+    <filth>
+        <placementMask> <!-- enum (flag) type -->
+        <li>Terrain</li>
+        <li>Unnatural</li>
+        </placementMask>
+    </filth>
+    </ThingDef>
+</Defs>
+`
+
+    const doc = parse(xml)
+
+    const defsNode = $(doc).find('Defs').get(0) as unknown as Element
+
+    expect(defsNode).toBeDefined()
+  })
+})
